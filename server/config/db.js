b746442382ed15e90524642bd46b46f09dae41f3
Abdul Basith <116253542+basith4me@users.noreply.github.com>
2024-09-18 17:06:55 +0530

    initial commit with API

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`mongodb connected: ${conn.connection.host} `);
+  } catch (error) {
+    console.error(`error message: ${error.message}`);
+    // process.exit(1);
+  }
+};
