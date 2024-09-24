0744fe373cbc0c9a2aea2586532d78432bb8b585
Abdul Basith <116253542+basith4me@users.noreply.github.com>
2024-09-24 14:06:05 +0530

    completed server development

--- server/server.js
+++ server/server.js
@@ -1,34 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
-import Product from "./models/products.models.js";
+import ProductRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT
 app.use(express.json());
-app.post("/api/products", async (req, res) => {
-  const product = req.body;
-
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "please provide all fields" });
-  }
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("error in create product", error.message);
-    res.status(500).json({ success: false, message: "server error" });
-  }
-});
-
-app.listen(5000, () => {
+app.use("/api/products", ProductRoutes);
+app.listen(PORT, () => {
   connectDb();
-  console.log("server started at http://localhost:5000");
+  console.log("server started at http://localhost:", PORT);
 });
-
-
